12e562c132fcaf6ae3e2ecf1c6354c25da0a656f
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2014-01-23 12:46:52 +0100

    Add basic tree traversal
    Signed-off-by: Maximilian Marx <mmarx@wh2.tu-dresden.de>

--- Main.hs
+++ Main.hs
@@ -4,11 +4,20 @@ module Main (main) where
 
 import Prelude hiding (FilePath)
 
+import Data.Conduit ( ($$)
+                    , (=$)
+                    , runResourceT
+                    )
+import qualified Data.Conduit.List as CL
+import Data.Conduit.Filesystem ( sourceFile
+                               , traverse
+                               )
 import Data.Monoid ((<>))
 import Data.Text ( Text
                  , pack
                  , unpack
                  )
+import qualified Data.Text.IO as TIO
 import System.Environment (getArgs)
 import Filesystem.Path.CurrentOS ( fromText
                                  , toText
@@ -25,10 +34,19 @@ needles = [ ("Datatypes", [])
           , ("HasKey", ["HasKey"])
           ]
 
+statFile :: FilePath -> IO ()
+statFile path = case toText path of
+  Left p -> error . unpack $ "could not decode path `" <> p <> "'"
+  Right p -> do
+    TIO.putStrLn $ "got file `" <> p <> "'"
+    return ()
+
 treeStat :: FilePath -> IO ()
 treeStat path = case toText path of
-  Left p -> putStrLn . unpack $ "failed to decode path `" <> p <> "'"
-  Right p -> putStrLn . unpack $ "got path `" <> p <> "'"
+  Left p -> error . unpack $ "could not decode path `" <> p <> "'"
+  Right p -> do
+    TIO.putStrLn $ "traversing `" <> p <> "'"
+    traverse True path $$ CL.mapM_ statFile
 
 main :: IO ()
 main = getArgs >>= mapM_ (treeStat . fromText . pack)
